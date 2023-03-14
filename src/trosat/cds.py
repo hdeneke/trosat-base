420a34d5142d9dbf8774c52210cc6111af5d5397
hdeneke <deneke@tropos.de>
2023-03-14 16:43:34 +0100

    trosat.cds: add functions for reading and expanding CDS requests.

--- src/trosat/cds.py
+++ src/trosat/cds.py
@@ -20,16 +20,142 @@ References:
 * https://cds.climate.copernicus.eu/modules/custom/cds_apikeys/app/apidocs.html
 '''
 
+import jstyleson as json
 import os
-import sys
-import json
-
 import requests
+import string
+import sys
+
+from collections.abc import Mapping
 from requests.compat import urljoin
+from toolz import dicttoolz
+
+def request_has_children(req):
+    ''' Test if request is a parent request '''
+    return "request_children" in req
+
+
+def read_requests(fn, *, expand_user=True, expand_vars=True, \
+                  expand_children=True, obj_hook=dict        ):
+    '''
+    Read CDS request(s) from JSON-formatted file
+    
+    Parameters
+    ----------
+    fpath : str or PathLike
+        the file path to the JSON file
+    expand_user : bool, default=True
+        tilde/user home directory expansion of target
+        key, by invoking os.path.expanduser
+    expand_vars : bool, default=True
+        environment variable expansion of target key,
+        by invoking os.path.expanduser
+    expand_children : bool, default=False
+        expand child requests
+    obj_hook : Mapping, default=dict
+        class of mapping type
+
+    Returns
+    -------
+    reqs : list 
+        list of requests
+    '''
+
+    def _read_json(fpath, *, obj_hook=dict):
+        with open(fpath,"r") as f:
+            js = json.load(f, object_hook=obj_hook)
+            return js
+    
+    def _expand_path(p, user=True, vars=True):
+        p = os.path.expanduser(p) if user else p
+        p = os.path.expandvars(p) if vars else p
+        return p
+
+    # read JSON file
+    reqs = _read_json(fn, obj_hook=obj_hook)
+
+    # support both single request and list of requests
+    # => convert reqs to list if necessary
+    if isinstance(reqs, Mapping):
+        reqs = [reqs]
+    elif isinstance(reqs, Sequence):
+        pass
+    else:
+        raise ValueError("Read neither Sequence nor Mapping")
+
+    # expand path
+    if expand_user or expand_vars:
+        for r in reqs:
+            if "target" in r:
+                r["target"] = _expand_path(
+                    r["target"],
+                    user=expand_user,
+                    vars=expand_vars
+                )
+
+    # expand children requests
+    if expand_children:
+        _reqs = []
+        for r in reqs:
+            if request_has_children(r):
+                children = r.pop("request_children")
+                _reqs.extend([{**r, **ch} for ch in children])
+            else:
+                _reqs.append(r)
+            reqs = _reqs
+
+    # finally return
+    return reqs
+
+### misc. private objects/functions
+
+# string.Formatter object
+_fmtr = string.Formatter()
+
+# test function if s is a string
+_is_str = lambda s:  isinstance(s, str)
+
+def _fmt_filter(s):
+    ''' test if s is a format string with named fields '''
+    if _is_str(s):
+        return _is_str(tuple(_fmtr.parse(s))[0][1])
+    else:
+        return False
+
+
+def _fmt_request(r, farg, fmt_keys):
+    ''' for requests, map values to str.format output for keys in fmt_keys  '''
+    _fmt_map = lambda i: (i[0],i[1].format(req=r, **farg)) if i[0] in fmt_keys else i
+    return dicttoolz.itemmap(_fmt_map, r)
+
+
+def expand_requests(base_reqs, farg_iter, *, fmt_keys=None):
+    '''
+    Expand list of CDS request templates by substitution of request values
+    with format strings by str.format output, iterating over format arguments
+    
+    Parameters
+    ----------
+    base_reqs : sequence of mapping
+        a sequence of request templates
+    farg_iter : sequence of mapping
+        a sequence of named key-word arguments for str.format
+    fmt_keys : set of str, default=None
+        the request keys who's values contain format strings
+        
+    Returns
+    -------
+    req_gen : iterator of mapping
+        an iterator of requests
+    '''
+    for farg in farg_iter:
+        for i,req in enumerate(base_reqs):
+            _keys = fmt_keys or set(dicttoolz.valfilter(_fmt_filter, req).keys())
+            yield _fmt_request(req.copy(), farg, _keys)
+
 
 class session(requests.Session):
-
-'''
+    '''
     Session class for sending HTTP requests to the Copernicus Data
     Store (CDS) API endpoints.
     
